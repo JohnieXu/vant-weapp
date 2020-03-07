ed905e826533cd437d458e0cefc26c9353715da4
JohnieXu <281910378@qq.com>
2020-03-08 00:48:36 +0800

    feat: 首页整体更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,24 +7,61 @@ const app = getApp()
 Page({
   data: {
     username: 'JohnieXu',
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    modules: [
+      {
+        title: '进阶',
+        link: '',
+      },
+      {
+        title: '精选',
+        link: '',
+      },
+      {
+        title: 'demo',
+        link: '',
+      },
+      {
+        title: '全部',
+        link: '',
+      },
+      {
+        title: '推荐',
+        link: '',
+      },
+      {
+        title: '关于',
+        link: '',
+      },
+    ],
+    subscription: {
+      qrcode: 'https://tva1.sinaimg.cn/large/00831rSTgy1gclpr3746wj304s04sglu.jpg',
+      slogan: '个人前端开发实战经验分享，一同学习提示'
+    },
+    wechatIcon: 'https://tva1.sinaimg.cn/large/00831rSTly1gclqf08h4aj301100udfl.jpg',
   },
-  // 事件处理函数
-  bindViewTap: async function() {
-    Toast('我是提示文案，建议不超过十五字~')
-    let res = {}
-    try {
-      res = await login('admin', '123123')
-    } catch (e) {
-      console.error(e)
+  // 模块点击
+  handleModuleTap: (e) => {
+    const title = e.target.dataset.title
+    if (!title) {
+      return
     }
-    console.log(res)
+    switch (title) {
+      case 'demo':
+        wx.navigateTo({
+          url: '/pages/dialog/index/index',
+        })
+        break
+      default:
+        Toast('小哥哥正在全力开发中^_^')
+        break
+    }
   },
   // 头部更新点击
   bindMoreTap: function() {
-    wx.navigateTo({
-      url: '/pages/mine/mine',
-    })
+    // wx.navigateTo({
+    //   url: '/pages/mine/mine',
+    // })
   },
   onLoad: function () {},
 })
