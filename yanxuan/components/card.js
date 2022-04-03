4e153a43c1e2ebd80f2a60b96fa9586e057953a3
JohnieXu <281910378@qq.com>
2022-04-03 22:08:18 +0800

    feat: 严选模块添加 c-card 组件

---
+++ yanxuan/components/card.js
@@ -0,0 +1,26 @@
+// yanxuan/components/card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    span: {
+      type: Number | String,
+      default: 12
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
