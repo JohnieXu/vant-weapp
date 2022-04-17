25534a6840b5609908f95e787a655db3dc732558
JohnieXu <281910378@qq.com>
2022-04-17 11:37:44 +0800

    feat: 更新严选首页页面

---
+++ yanxuan/components/home_like_list/index.js
@@ -0,0 +1,30 @@
+// yanxuan/components/home_like_list/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    list: {
+      type: Array
+    },
+    type: String,
+    column: {
+      type: Number,
+      default: 2
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
